0ddbea9b74527afa0c57a0d48265171313131b4c
Glen Buck <glenbuck503@gmail.com>
2019-02-17 02:17:24 -0800

    step124

---
+++ Step124/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person persons = new Employee();
+            //persons.FirstName = "Sample";
+            //persons.LastName = " Student";
+            //persons.SayName();
+            //Console.ReadLine();
+
+            Employee persons = new Employee();
+            persons.FirstName = "Sample";
+            persons.LastName = " Student";
+            persons.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
